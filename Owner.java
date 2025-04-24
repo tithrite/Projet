0aed32985c187b52fe696fe1b7b15258277d2a9d
astFat <FATIMAZZAHRAE.ASTITOU.ESTN24@ump.ac.ma>
2025-04-24 01:37:14 +0100

    Add files via upload

---
+++ Owner.java
@@ -0,0 +1,108 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Owner {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String phone;
+    private String email;
+    private List<Pet> pets;
+
+    public Owner() {
+        this.pets = new ArrayList<>();
+    }
+
+    public Owner(int id, String firstName, String lastName, String address, String city, String phone, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.phone = phone;
+        this.email = email;
+        this.pets = new ArrayList<>();
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public void addPet(Pet pet) {
+        pets.add(pet);
+    }
+
+    public void removePet(Pet pet) {
+        pets.remove(pet);
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
